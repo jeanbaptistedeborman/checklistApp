f3c588a2343f03234cec4a414f3b062bf69524e7
jeanbaptistedeborman <jb@noloading.com>
2013-11-06 15:47:48 +0100

    The process manages steps

--- scripts/jb/application/checklistapp/application.js
+++ scripts/jb/application/checklistapp/application.js
@@ -7,34 +7,39 @@
 
 var Application = {
 	question_num : 1000,
+	questionsTotal_num : 1000,
+	step_num:0, 
 	display_$ : null,
 	progressPointContainer_$ : null,
+	application:null,
+	steps_$:null,  
 
 	QuestionManager : function() {"use strict";
 		var context = this;
-
+		this.conditionMet_bool = false;
+		this.lastAnswer_str = "default";
 		this.onChoiceMade = function() {
 
 			// Event placeholder;
 
 		};
 		this.display = function(question_$) {
-			//trace(question_$.html());
+
 			Application.display_$.append(question_$);
 			var responses_$ = question_$.find(".response");
-			//responses_$.hide ();
+
 			responses_$.on("click", function() {
-				trace("hello");
 				var this_$ = $(this);
-				var met_bool = this_$.attr("data-result") !== 'none';
+				context.conditionMet_bool = this_$.attr("data-result") !== 'none';
 				var currentProgressPoint_$ = $(".active");
 				currentProgressPoint_$.toggleClass("active");
-				if (met_bool) {
-					currentProgressPoint_$.toggleClass("met");
+				context.lastAnswer_str = this_$.text();
+				if (context.conditionMet_bool) {
+					currentProgressPoint_$.toggleClass("conditionMet");
 
 				} else {
 
-					currentProgressPoint_$.toggleClass("notMet");
+					currentProgressPoint_$.toggleClass("conditionNotMet");
 
 				}
 
@@ -49,41 +54,84 @@ var Application = {
 	StepManager : function(application, step_$) {"use strict";
 
 		var questions_$, question_$, responses_$, n;
-		var displayQuestion = function(question_num) {
-			
-			$('body .question, body .response').remove (); 
-			trace (question_num); 
-			
-			var question_$ = $(questions_$[question_num]);
-			var questionManager = new Application.QuestionManager();
-			$($(".progressPoint")[question_num]).toggleClass('active');
-			questionManager.display(question_$);
-			
-			
-			
-			
-			questionManager.onChoiceMade = function() {
-				 
 
-				displayQuestion(++application.question_num);
-				//trace("onChoicemade");
-
-			};
-
-			//Application.progressPointContainer_$.
+		var displayNextStep = function() {
 
 		};
 
+		var displayQuestion = function(question_num) {
+
+			var question_$ = $(questions_$[question_num]);
+			var questionManager = new Application.QuestionManager();
+			$($(".progressPoint")[question_num]).toggleClass('active');
+			var timeout = setTimeout(function() {
+				questionManager.display(question_$);
+			}, 1500);
+
+			questionManager.onChoiceMade = function() {
+
+				$('body .question, body .response').remove();
+				trace(questionManager.lastAnswer_str + "   /   " + this.lastAnswer_str);
+
+				var timeout = setTimeout(function() {
+					trace(questionManager.lastAnswer_str + "   /   " + this.lastAnswer_str);
+
+					if (!questionManager.conditionMet_bool) {
+
+						if (++application.question_num >= application.questionsTotal_num) {
+
+							application.display_$.append(step_$.find(".endScreen .conditionNotMet"));
+						
+
+						} else {
+							displayQuestion(application.question_num);
+							
+
+						}
+					} else {
+
+						var feedBack_$ = $(step_$.find(".endScreen .conditionMet .feedBack"));
+						feedBack_$.text(feedBack_$.text().replace("%result%", questionManager.lastAnswer_str));
+
+						application.display_$.append(feedBack_$.parent());
+						
+						feedBack_$.parent().find ('.button').on ("click", function () {
+							application.nextStep (); 
+							
+							
+						}); 
+							
+						
+					
+						
+
+					}
+				}, 1000);
+
+			};
+
+		};
+		
+		var nextStep = function () {
+			
+			displayQuestion (++application.question_num); 
+			
+		}; 
+
 		var init = function(step_$) {
-			//var application =this;
 
 			var name_$ = step_$.find(".name"), circle_params = new SVGFactory.Params();
 			questions_$ = step_$.find(".questionSet");
 			application.question_num = 0;
+			application.questionsTotal_num = questions_$.length;
 
 			for ( n = 0; n < questions_$.length; n++) {
 				var progressPoint_$ = $("<span></span>");
+				var textSpan_$ = $("<span></span>");
+				textSpan_$.text(n + 1);
+
 				progressPoint_$.append(SVGFactory.getShape(circle_params));
+				progressPoint_$.append(textSpan_$);
 				progressPoint_$.addClass("progressPoint");
 				application.progressPointContainer_$.append(progressPoint_$);
 			}
@@ -93,34 +141,51 @@ var Application = {
 
 		};
 
-		init($(step_$));
+		init(step_$);
 
 	},
-
-	init: function(display_$) {"use strict";
-		this.display_$ = display_$;
+	nextStep : function (){"use strict";
+	$('body').empty (); 
+		
+		
+		trace ("nextstep"); 
+		
 		this.progressPointContainer_$ = $('<div id="progressPointContainer"></div>');
-
 		this.display_$.append(this.progressPointContainer_$);
-		var steps_$ = $('.step');
-		$(".questionSet").each (function (index, element) {
-			trace (index); 
-			var question_$ = $(element).find ('.question');
-			question_$.text (Number (1+ index) +". " + question_$.text ()); 
-			
-			
-			
-		}); 
 		
-		//$(".questionSet").hide (); 
-			
-			
-		
+		var stepManager = new this.StepManager(this, $(this.steps_$[this.step_num]));
+		this.step_num++; 
+		 
+	}, 
 
-		steps_$.detach();
+	init : function(display_$) {"use strict";
+		var application = this;
+		/* SET UP */
+		this.steps_$ = $('.step');
+		
+		
+		this.display_$ = display_$;
+		
 	
 
-		var stepManager = new this.StepManager(this, steps_$[0]);
+		$(".step").each(function(index, element) {
+
+		 var question_$ = $(element).find('.name');
+		 question_$.text(Number(1 + index) + ". " + question_$.text());
+
+		 });
+	
+
+		this.steps_$.detach();
+		
+
+		/*INTRO SCREEN */
+
+		$('#intro .button').on("click", function() {
+			$('#intro').detach();
+			application.nextStep (0); 
+
+		});
 
 	}
 };
